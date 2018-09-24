ef0b0db55808dd3c2ad1798d92e350b4ecdb675a
Thomas Hanson <thomasbhanson@gmail.com>
2018-09-24 13:25:35 +0100

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,148 @@
+$(document).ready(function() {
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').removeClass('slideOutUp');
+            $('nav').addClass('sticky animated slideInDown');
+        } else {
+            $('nav').removeClass('slideInDown');
+            $('nav').addClass('animated slideOutUp');
+        }
+    },{
+        offset: '84px;'
+    });
+    
+    $('.js--main-nav').waypoint(function(direction) {
+        if (direction == "up") {
+            $('nav').removeClass('sticky animated slideInDown slideOutUp');
+        }
+    },{
+        offset: '-120px;'
+    });    
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    /* Mobile navigation */
+    /* $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-remove-round');
+        }
+        
+    }) */
+    
+    $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+            return;
+
+        //Opens and closes the menu
+        if ($(window).width() < 768){
+            nav.slideToggle(200);
+        }
+
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+    /* Keep navigation on window resize */
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
+    
+    /* Maps */
+    
+    var map = new GMaps({
+        div: '.map',
+        lat: 38.7436883,
+        lng: -9.05,
+        zoom: 12,
+    });
+    
+    map.addMarker({
+        lat: 38.7436883,
+        lng: -9.1952227,
+        title: 'Lisbon',
+        infoWindow: {
+        content: '<p>Our Lisbon HQ</p>'
+        }
+    });
+    
+});
